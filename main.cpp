2faa0f67a12db1ba9ce0c0d135e98a71a9b10a4a
missqu <missquprisoner@gmail.com>
2026-09-04 20:27:13 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include<pqxx/pqxx>
+#include<iostream>
+//localhost:5432
+//127.0.0.1:5432 - SQL server
+//Milestone 0.5: connected to server db and told it to return 1 to me and it did :O[x]
+//Milestone 1: recreate file reading with server based table[x]
+//Milestone 2: simple cli credentials on success, db sends back data, print data[x]
+
+//milestone 3: 
+int main()
+{
+    constexpr const char* server_addr = "postgresql://postgres:mleko1235@127.0.0.1:5432/auth_service";
+    try
+    {
+        pqxx::connection cx{server_addr};
+
+        pqxx::work tx{cx}; //transaction
+        std::cout << "Enter username:";
+        std::string username;
+        std::cin >> username;
+        std::cout << "Enter password:";
+        std::string password;
+        std::cin >> password;
+
+        auto result= tx.query<std::string,std::string>("select * from public.userbase where username=$1",pqxx::params{tx,username});
+        for (auto[username,password] : result)
+        {
+            std::cout << "Username:" << username << ", passwd:" << password << "\n";
+        }
+        tx.commit();
+
+        // std::cout << "Db returned" << row[0].as<int>() << std::endl;
+    }
+    catch (std::exception const& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
---
+++ sql/init.sql
@@ -0,0 +1,13 @@
+-- sql/init.sql
+--schema version 0.1
+CREATE TABLE IF NOT EXISTS accounts(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(32) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now());
+
+CREATE TABLE IF NOT EXISTS session (
+    id SERIAL PRIMARY KEY,
+    account_id INT NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+    token_hash TEXT UNIQUE NOT NULL,
+    expires_at TIMESTAMPTZ NOT NULL);
